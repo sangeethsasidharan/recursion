db20d1dd1a1cf49cb3caff9c8a2d15e6befd3f2a
SANGEETH.SASIDHARAN <sangeeth472@gmail.com>
2021-08-26 15:10:51 +0530

    palindrome and decimal to binary

--- src/DecimalToBinary.py
+++ src/DecimalToBinary.py
@@ -1,15 +1,16 @@
 
 class DecimalToBinary:
     def to_binary(self, number):
-        return int(self.calculate_binary(number))
+        result = ''
+        return int(self.calculate_binary(number, result))
 
-    def calculate_binary(self, number):
+    def calculate_binary(self, number, result):
+        if number == 0:
+            return result
+        result = str(number % 2) + result
 
-        if number // 2 == 0:
-            return str(number % 2)
-
-        return self.calculate_binary(number//2) + str(number % 2)
+        return self.calculate_binary(number//2, result)
 
 
 obj = DecimalToBinary()
-print(obj.to_binary(12322))
+print(obj.to_binary(12))
